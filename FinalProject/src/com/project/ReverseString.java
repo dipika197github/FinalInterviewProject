84bbff7bd0cb03573ee431f16b5bedc174069d73
dipika197github <dipikazade25@gmail.com>
2023-10-30 17:48:53 +0530

    added reverse string program

---
+++ FinalProject/src/com/project/ReverseString.java
@@ -0,0 +1,12 @@
+package com.project;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String s="dipika";
+		char[] ch=s.toCharArray();
+		for(int i=ch.length-1;i>=0;i--) {
+			System.out.println(ch[i]);
+		}
+	}
+}
